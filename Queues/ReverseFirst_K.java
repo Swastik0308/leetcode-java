04382f24ae3f3dcc13889d0d379545d772d84c4a
Swastik Shetty <145961827+Swastik0308@users.noreply.github.com>
2026-01-08 18:43:47 +0530

    Implement method to reverse first K elements of queue

---
+++ Queues/ReverseFirst_K.java
@@ -0,0 +1,21 @@
+class Solution {
+    public Queue<Integer> reverseFirstK(Queue<Integer> q, int k) {
+        // code here
+         if (q == null || q.isEmpty() || k <= 0 || k > q.size()) {
+            return q;
+        }
+        int size = q.size();
+        Stack<Integer> s = new Stack<>();
+        for(int i=0; i<k; i++){
+            s.push(q.remove());
+        }
+        while(!s.isEmpty()){
+            q.add(s.pop());
+        }
+        
+        for(int i=0; i<size-k; i++){
+            q.add(q.remove());
+        }
+        return q;
+    }
+}
